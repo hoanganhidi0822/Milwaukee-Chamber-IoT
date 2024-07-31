326e63940e1f5ee0d89ea8023e5a51f835686d6d
Phan Van Hoang Anh <hoanganh2282003@gmail.com>
2024-07-31 08:54:21 +0700

    first commit

--- ESP_IDF_CHAMBER/src/main.cpp
+++ ESP_IDF_CHAMBER/src/main.cpp
@@ -22,14 +22,14 @@
 #define ECHO_UART_PORT          (2)
 #define ECHO_READ_TOUT          (3)
 
-const char* ssid = "TTIIoT";
-const char* password = "TTiVNIoT2023";
+const char* ssid     = "TTi Factory";
+const char* password = "ttiFactory19@1";
 
 const char* mqtt_server = "10.147.37.59";
-const int mqtt_port = 1883;
-const char* mqtt_user = "hoanganh";
-const char* mqtt_pass = "123";
-const char* status_topic = "VNTESTLAB/CHAMBER/STATUS";
+const int   mqtt_port   = 1883;
+const char* mqtt_user   = "hoanganh";
+const char* mqtt_pass   = "123";
+const char* status_topic      = "VNTESTLAB/CHAMBER/STATUS";
 const char* daily_total_topic = "VNTESTLAB/CHAMBER/TOTAL";
 static bool retained_message_processed = false;
 char boardID[23];
@@ -40,12 +40,12 @@ char boardID[23];
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-static bool machine_running = false;
-static bool last_machine_running = false;
-static unsigned long start_time = 0;
+static bool machine_running             = false;
+static bool last_machine_running        = false;
+static unsigned long start_time         = 0;
 static unsigned long total_working_time = 0;
-static unsigned long last_update_time = 0;
-static unsigned long pre_time = 0;
+static unsigned long last_update_time   = 0;
+static unsigned long pre_time           = 0;
 struct tm timeinfo;
 
 int status = 1;
@@ -142,17 +142,17 @@ void processModbusAscii(const uint8_t *hex_array, size_t len, float *temp_pv, fl
     }
     ascii_str[len] = '\0';
 
-    char temp_pv_hex[5] = {ascii_str[7], ascii_str[8], ascii_str[9], ascii_str[10], '\0'};
+    char temp_pv_hex[5] = {ascii_str[7] , ascii_str[8] , ascii_str[9] , ascii_str[10], '\0'};
     char temp_sp_hex[5] = {ascii_str[11], ascii_str[12], ascii_str[13], ascii_str[14], '\0'};
     char humi_pv_hex[5] = {ascii_str[23], ascii_str[24], ascii_str[25], ascii_str[26], '\0'};
     char humi_sp_hex[5] = {ascii_str[27], ascii_str[28], ascii_str[29], ascii_str[30], '\0'};
-    char status_hex[5] = {ascii_str[43], ascii_str[44], ascii_str[45], ascii_str[46], '\0'};
+    char status_hex [5] = {ascii_str[43], ascii_str[44], ascii_str[45], ascii_str[46], '\0'};
 
     *temp_pv = hexStringToDec(temp_pv_hex) / 100.0;
     *temp_sp = hexStringToDec(temp_sp_hex) / 100.0;
     *humi_pv = hexStringToDec(humi_pv_hex) / 10.0;
     *humi_sp = hexStringToDec(humi_sp_hex) / 10.0;
-    *status = hexStringToDec(status_hex);
+    *status  = hexStringToDec(status_hex );
 }
 
 void update_mqtt() {
@@ -160,19 +160,18 @@ void update_mqtt() {
     unsigned long working_time = 0;
 
     //-------------------------------------------//
-
     if (machine_running) {
         working_time = current_time - start_time;
     }
     working_time = working_time + pre_time;
-    
-    //Serial.println(working_time);
+    //------------------------------------------//
+
     char payload[300];
     snprintf(payload, sizeof(payload), "{\"boardID\":\"%s\",\"temp_pv\":%.2f,\"temp_sp\":%.2f,\"humi_pv\":%.2f,\"humi_sp\":%.2f,\"status\":\"%d\",\"working_time\":\"%lu\"}",
              boardID, temp_pv, temp_sp, humi_pv, humi_sp, machine_running, working_time);
-    
     client.publish(status_topic, payload,true);
     
+    //------------------------------------------//
     if (machine_running != last_machine_running){
         a++;
         if (a == 5 && machine_running == false){
@@ -184,12 +183,12 @@ void update_mqtt() {
             //client.publish(daily_total_topic, daily_total_payload);
             client.publish(status_topic, payload, true);
             delay(100); 
-            
         }
     }
     last_machine_running = machine_running;
-    
-    //
+    //---------------------------------------------------//
+
+    //-----------------Update daily time ----------------//
     if (timeinfo.tm_hour == 23 && timeinfo.tm_min == 59) {
         
         char daily_total_payload[70];
@@ -200,20 +199,18 @@ void update_mqtt() {
         snprintf(payload, sizeof(payload), "{\"boardID\":\"%s\",\"temp_pv\":%.2f,\"temp_sp\":%.2f,\"humi_pv\":%.2f,\"humi_sp\":%.2f,\"status\":\"%d\",\"working_time\":\"%lu\"}",
              boardID, temp_pv, temp_sp, humi_pv, humi_sp, machine_running, 0);
     
-
         client.publish(status_topic, payload, true);
         client.publish(status_topic, payload, true);
         delay(100);
+
         total_working_time = 0;
+        working_time = 0;
         pre_time = 0; // Reset pre_time for the next day
         a = 0;
-        //saveTimeToEEPROM(0);
-        ESP.restart();
-        
+        ESP.restart();   
     }
-    
-
     last_update_time = current_time;
+    //----------------------------------------------------//
 }
 
 void echo_task(void *arg) {
@@ -229,11 +226,10 @@ void echo_task(void *arg) {
 
     esp_log_level_set(TAG, ESP_LOG_INFO);
 
-    ESP_LOGI(TAG, "Starting RS485 application and configuring UART.");
-
+    //ESP_LOGI(TAG, "Starting RS485 application and configuring UART.");
     ESP_ERROR_CHECK(uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
-    ESP_LOGI(TAG, "UART pins set, mode configured, and driver installed.");
+    //ESP_LOGI(TAG, "UART pins set, mode configured, and driver installed.");
 
     ESP_ERROR_CHECK(uart_set_pin(uart_num, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
     ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_RS485_HALF_DUPLEX));
@@ -243,20 +239,11 @@ void echo_task(void *arg) {
     while (1) {
         const char* command_data = ":01030000000AF2\r\n";
         echo_send(uart_num, command_data, strlen(command_data));
-        int len = uart_read_bytes(uart_num, data, BUF_SIZE, PACKET_READ_TICS);
-        
+        int len = uart_read_bytes(uart_num, data, BUF_SIZE, PACKET_READ_TICS);  
         processModbusAscii(data, len, &temp_pv, &temp_sp, &humi_pv, &humi_sp, &status);
 
-        //------Test Value------//
-        /* status = 2;
-        temp_pv = -40.22;
-        temp_sp = -40.00;
 
-        humi_pv = 13.82;
-        humi_sp = 13.00; */
-        //----------------------//
-        
-        
+        //-------Update device status-------//
         if (status == 4 && !machine_running) {
             machine_running = true;
             start_time = millis() / 1000;
@@ -266,15 +253,17 @@ void echo_task(void *arg) {
             machine_running = false;
         }
 
+        //---Get local time--------//
         if(!getLocalTime(&timeinfo)){
             Serial.println("Failed to obtain time") ;
             return;
         }
+
+        //--------------Update time----------------//
         unsigned long current_time = millis() / 1000;
         if (current_time - last_update_time >= 59) {
             update_mqtt();
         }
-
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
@@ -294,7 +283,7 @@ void setup() {
     delay(10);
 
     //---------Set up time synchronization-----------//
-    configTime(7 * 3600, 0, "CNSDC01.cn.globaltti.net");
+    configTime(7 * 3600, 0, "10.147.37.59");
     while (!time(nullptr)) {
         Serial.println("Waiting for time sync...");
         delay(2000);
@@ -303,21 +292,21 @@ void setup() {
     //-----------------------------------------------//
 
     xTaskCreatePinnedToCore(
-        echo_task,          /* Task function. */
-        "echo_task",        /* name of task. */
+        echo_task,                 /* Task function. */
+        "echo_task",               /* name of task. */
         ECHO_TASK_STACK_SIZE,      /* Stack size of task */
-        NULL,               /* parameter of the task */
-        ECHO_TASK_PRIO,     /* priority of the task */
-        NULL,               /* Task handle to keep track of created task */
-        1);                 /* pin task to core 1 */
+        NULL,                      /* parameter of the task */
+        ECHO_TASK_PRIO,            /* priority of the task */
+        NULL,                      /* Task handle to keep track of created task */
+        1);                        /* pin task to core 1 */
         
 }
-
 void loop() {
     if (!client.connected()) {
         reconnect();
     }
-    client.loop();
-    
+
+    client.loop(); 
+    //Serial.println(timeinfo.tm_hour);
 }
 
